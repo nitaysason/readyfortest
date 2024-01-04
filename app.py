2b8172d99d4d81a34561269208e3848e12052263
nitaysason <nitaysason@gmail.com>
2024-01-05 00:27:23 +0200

    first commit

--- app.py
+++ app.py
@@ -2,18 +2,20 @@ from aaa.bbb.samp import calc  # Exercise 1,  line 1-2(There are two files, blab
 from aaa.blabla import check_array, waga
 
 def main():
-    result_calc = calc(5) # Exercise 2 , line 5-12
-    print(result_calc) if should_call_calc_first() else print("Error: Please call calc before waga")
+    result_calc = calc(5) # Exercise 2 , line 5-14
+    if should_call_calc_first(): 
+       print(result_calc) 
+    else : print("Error: Please call calc before waga")
 
     if should_call_calc_first():
         print(waga())
 
 def should_call_calc_first():
-    return True   # Change this to False to see different behavior
+    return False   # Change this to True to see different behavior
 
-print(check_array([2, 3, 5])) # Exercise 3 line 14
+print(check_array([2, 3, 5])) # Exercise 3 line 16
 
-def sort_array(arr): # Exercise 4 , line 16-22 example  1
+def sort_array(arr): # Exercise 4 , line 18-24 example  1
     return sorted(arr)
 
 
@@ -22,7 +24,7 @@ sorted_array = sort_array(my_array)
 print(sorted_array)
 
 
-def sort_array_in_place(arr): # Exercise 5 , line 25-32 example  2
+def sort_array_in_place(arr): # Exercise 5 , line 27-34 example  2
     arr.sort()
     print(arr)
     return arr
